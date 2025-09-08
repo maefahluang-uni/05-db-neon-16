9704f9e5d114608aed12601c3672b9895dae9c75
nyanlinn-h <6731503076@lamduan.mfu.ac.th>
2025-09-08 09:05:34 +0000

    done!

--- src/main/java/th/mfu/boot/UserController.java
+++ src/main/java/th/mfu/boot/UserController.java
@@ -27,29 +27,35 @@ public class UserController {
     public ResponseEntity<String> registerUser(@RequestBody User user) {
 
         //TODO: check if user with the username exists
-       
+        if(repo.findByUsername(user.getUsername())!=null)
+        {
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
         //TODO: save the user
-
+            repo.save(user);
         //TODO: remove below and return proper status
-        return new ResponseEntity<>( HttpStatus.NOT_IMPLEMENTED);
+        return new ResponseEntity<>(HttpStatus.CONFLICT);
     }
 
     @GetMapping("/users")
     public ResponseEntity<List<User>> list() {
         
         //TODO: remove below and return proper result
-        return new ResponseEntity<>( HttpStatus.NOT_IMPLEMENTED);
+        return new ResponseEntity<List<User>> (repo.findAll(),HttpStatus.OK);
     }
 
     @DeleteMapping("/users/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable Long id) {
         
         //TODO: check if user with the id exists
-       
+        if(repo.findById(id) != null)
         //TODO: delete the user
-    
+        {
+            repo.deleteById(id);
+            return new ResponseEntity<String>(HttpStatus.OK);
+        }
         //TODO: remove below and return proper status
-        return new ResponseEntity<>( HttpStatus.NOT_IMPLEMENTED);
+        return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
     }
 
 
